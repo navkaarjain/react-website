9fc5a27dada14d8979b8ade8becc4778c912bac8
navkaarjain <navkaarjain96@gmail.com>
2020-12-18 19:02:37 +0530

    Updates

--- src/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Common from './Common';
-import web from '../src/images/img1.jpg';
-const Home=()=>{
-  return (<>
- <Common  name='Grow your business with' imgsrc={web} visit='/services' btname='get started' />
-  </>);
-};
-export default Home;
